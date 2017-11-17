b07f956974b5df63207928abaeedfd68f352c5b9
MindeVieras <wieraz21@gmail.com>
2017-11-17 00:08:11 +0000

    user validate and create done

--- models/users.js
+++ models/users.js
@@ -5,64 +5,60 @@ var connection = require('../config/db');
 
 // Creates user
 exports.create = function(req, res){
-  // res.header("Access-Control-Allow-Origin", "*");
-  console.log(req.body);
-  // res.json({ack: 'err', msg: 'no service'});
 
-  let input = req.body;
-  
-  // // vlaidate form
+  // console.log(req.headers);
+
+  const input = req.body;
+  // // vlaidate input
   if (validator.isEmpty(input.username)) {
     res.json({ack:'err', msg: 'Username is required'});
-  }
-  if (validator.isLength(input.username, {min:0, max:4})) {
+  } else if (validator.isLength(input.username, {min:0, max:4})) {
     res.json({ack:'err', msg: 'Username must be at least 5 chars long'});
-  }
-  if (!validator.isEmail(input.email) && !validator.isEmpty(input.email)){
+  } else if (!validator.isEmail(input.email) && !validator.isEmpty(input.email)){
     res.json({ack:'err', msg: 'Email must be valid'});
-  }
-  if (validator.isEmpty(input.password)){
+  } else if (validator.isEmpty(input.password)){
     res.json({ack:'err', msg: 'Password is required'});
-  }
-  
-  const userId = input.id || false;
-  bcrypt.hash(input.password, 10, function(err, hash) {
-    if (err) {
-      res.json({ack:'err', msg: 'Cannot hash password'});
-    } else {    
-      let data = {
-          username : input.username,
-          email : input.email,
-          password: hash,
-          display_name: input.display_name,
-          access_level: input.access_level
-          // author: req.user.id,
-          // status: input.status
-      };
-      // res.json({ack:'ok', msg: 'Pass hashed', data: hash});
-      if (userId) {
-      //   //res.send(JSON.stringify(data));
-      //   delete data.author;
-      //   delete data.password;
-      //   connection.query('UPDATE users set ? WHERE id = ?', [data, userId], function(err,rows)     {
-                
-      //       if(err)
-      //         console.log('Error saving user : %s ',err );
-
-      //       res.send(JSON.stringify({ack:'ok'}));
-                               
-      //     });
+  } else if (validator.isLength(input.password, {min:0, max:4})) {
+    res.json({ack:'err', msg: 'Password must be at least 5 chars long'});
+  } else {
+    // check if user exists
+    connection.query('SELECT * FROM users WHERE username = ? LIMIT 1', [input.username], function(err, rows) {
+      if(err) {
+        res.json({ack:'err', msg: err.sqlMessage});
       } else {
-        connection.query('INSERT INTO users set ? ', data, function(err, rows) {
-            if(err) {
-              res.json({ack:'err', msg: err.sqlMessage});
-            } else {
-              res.json({ack:'ok', msg: 'User saved', id: rows.insertId});
+        if (rows.length) {
+          res.json({ack:'err', msg: 'Username already taken'});
+        } else {
+          // hashes password
+          bcrypt.hash(input.password, 10, function(err, hash) {
+            if (err) {
+              res.json({ack:'err', msg: 'Cannot hash password'});
+            } else {    
+              let data = {
+                  username : input.username,
+                  email : input.email,
+                  password: hash,
+                  display_name: input.display_name,
+                  access_level: input.access_level || 25,
+                  author: input.author || 0,
+                  status: input.status || 0
+              };
+
+              connection.query('INSERT INTO users set ? ', data, function(err, rows) {
+                  if(err) {
+                    res.json({ack:'err', msg: err.sqlMessage});
+                  } else {
+                    res.json({ack:'ok', msg: 'User saved', id: rows.insertId});
+                  }
+                });
             }
           });
+        }
       }
-    }
-  });
+    });
+  }
+  
+  
 };
 
 // Gets users
